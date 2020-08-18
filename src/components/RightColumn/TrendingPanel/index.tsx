902d541f41c7d3f46e719cc380c6e7100c52973d
Felipe <profissionalf.andre@gmail.com>
2020-08-18 20:44:18 -0300

    fix: remove duplicate ul and li in thrending right column

--- src/components/RightColumn/TrendingPanel/index.tsx
+++ src/components/RightColumn/TrendingPanel/index.tsx
@@ -1,58 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import Panel from '../../Panel';
+import Panel from "../../Panel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const TrendingPanel: React.FC = () => {
+  const arrayAssunts = [
+    {
+      name: "Aurora irá testar 11 mil funcionários...",
+      index: 1,
+      readers: Math.floor(Math.random() * 3000),
+    },
+    {
+      name: "Há vagas: confira as empresas",
+      index: 2,
+      readers: Math.floor(Math.random() * 3000),
+    },
+    {
+      name: "Interesse por videogames dispara na",
+      index: 3,
+      readers: Math.floor(Math.random() * 3000),
+    },
+    {
+      name: "Pesquisa detalha habitos dos brasileiros...",
+      index: 4,
+      readers: Math.floor(Math.random() * 3000),
+    },
+    {
+      name: "A importância do aprendizado contínuo na vida...",
+      index: 5,
+      readers: Math.floor(Math.random() * 3000),
+    },
+  ];
+
   return (
     <Container>
       <Panel>
         <span className="title">Assuntos mais discutidos</span>
 
         <ul>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
-          <li>
-            <span className="bullet" />
-            <span className="news">
-              <span className="head">Bootcamp da Rocketseat</span>
-              <span className="subtext">há 1 d • 316 leitores</span>
-            </span>
-          </li>
+          {arrayAssunts.map(({ index, name, readers }) => (
+            <li key={index}>
+              <span className="bullet" />
+              <span className="news">
+                <span className="head">
+                  <>
+                    <span>{name}</span>
+                    <br></br>
+                  </>
+                </span>
+                <span className="subtext">há 1 d • {readers} leitores</span>
+              </span>
+            </li>
+          ))}
         </ul>
       </Panel>
     </Container>
--- src/components/RightColumn/index.tsx
+++ src/components/RightColumn/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import TrendingPanel from './TrendingPanel';
+import TrendingPanel from "./TrendingPanel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const RightColumn: React.FC = () => {
   return (
     <Container className="right-column">
-      <TrendingPanel />
       <TrendingPanel />
     </Container>
   );
